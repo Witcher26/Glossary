f1e418ff36851ba715057357d4a9ab24e1be170f
Witcher26 <extreme8igor088@mail.ru>
2022-05-12 16:05:10 +0300

    Добавлены классы и интерфейс

--- src/main/java/ru/zvezdilin/javacore/Main.java
+++ src/main/java/ru/zvezdilin/javacore/Main.java
@@ -1,15 +1,22 @@
 package ru.zvezdilin.javacore;
 
-import ru.zvezdilin.javacore.classes.TodoServer;
-import ru.zvezdilin.javacore.classes.Todos;
+//import ru.zvezdilin.javacore.classes.TodoServer;
+//import ru.zvezdilin.javacore.classes.Todos;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Todos todos = new Todos();
-        TodoServer server = new TodoServer(8989, todos);
-        server.start();
+//        Todos todos = new Todos();
+//        TodoServer server = new TodoServer(8989, todos);
+//        server.start();
+        System.out.println("start");
+
+        String s1 = "d";
+        String s2=null;
+
+        System.out.println(Objects.equals(s1, s2));
     }
 }
