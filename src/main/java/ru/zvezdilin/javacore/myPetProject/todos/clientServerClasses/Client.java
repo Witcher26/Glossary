a142881ae42229e935ee559a9039449b6929c5f6
Witcher <[200~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~>
2022-05-24 15:41:34 +0300

    Добавлен клиент - пока только концепция. Надо переработать

--- src/main/java/ru/zvezdilin/javacore/myPetProject/todos/clientServerClasses/Client.java
+++ src/main/java/ru/zvezdilin/javacore/myPetProject/todos/clientServerClasses/Client.java
@@ -1,26 +1,51 @@
 package ru.zvezdilin.javacore.myPetProject.todos.clientServerClasses;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.Random;
 
 public class Client {
-    public static char pickRandomChar() {
-        String chars = "ABCDEFG";
-        return chars.charAt(new Random().nextInt(chars.length()));
-    }
+    private static final String name = "myPetProgect";
+    private static final String messFromServ = "Message from Server: ";
 
-    public static void main(String[] args) throws IOException {
-        try (
-            Socket socket = new Socket("localhost", 8989);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true)
-        ) {
-            out.println("{ \"type\": \"ADD\", \"task\": \"task #" + pickRandomChar() + "\" }");
-            System.out.println(in.readLine());
+    public static void main(String[] args) {
+
+        try {
+            try (Socket clientSocket = new Socket("localhost", 8181);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                 BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
+
+                System.out.printf("Подключение к серверу с портом %s ", clientSocket.getPort() + "\n");
+
+                String word = Integer.toString(clientSocket.getPort());
+                out.write(word + "\n");
+                out.flush();
+
+                String serverWord = in.readLine();
+                System.out.println(messFromServ + serverWord);
+
+                serverWord = in.readLine();
+                System.out.println(messFromServ + serverWord);
+                if (serverWord.equalsIgnoreCase("Write your name")) {
+                    out.write(name + "\n");
+                    out.flush();
+                }
+
+                serverWord = in.readLine();
+                System.out.println(messFromServ + serverWord);
+                if (serverWord.equalsIgnoreCase("Write your name")) {
+                    out.write("My name is MyPetProject" + "\n");
+                    out.flush();
+                }
+                out.write("yes" + "\n");
+                out.flush();
+
+                serverWord = in.readLine();
+                System.out.println(messFromServ + serverWord);
+            }
+
+        } catch (IOException e) {
+            System.err.println(e);
         }
     }
 }
